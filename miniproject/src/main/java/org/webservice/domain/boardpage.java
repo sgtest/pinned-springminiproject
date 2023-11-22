ea2d8d7d4fa43ce4bdfc02fd200d54d3801c8a46
somecreater <dbstjrgy123@gmail.com>
2023-11-22 21:59:42 +0900

    요구하는 도메인 수정

--- miniproject/src/main/java/org/webservice/domain/boardpage.java
+++ miniproject/src/main/java/org/webservice/domain/boardpage.java
@@ -1,5 +1,10 @@
 package org.webservice.domain;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class boardpage {
 	  private String boardname;
 	  private int startPage;
@@ -12,7 +17,19 @@ public class boardpage {
 	  public boardpage(boardsearch srh,int total) {
 		  this.srh=srh;
 		  this.total=total;
-		  
+		  this.endPage = (int) (Math.ceil(srh.getPageNum() / 10.0)) * 10;
+
+		    this.startPage = this.endPage - 9;
+
+		    int realEnd = (int) (Math.ceil((total * 1.0) / srh.getAmount()));
+
+		    if (realEnd < this.endPage) {
+		      this.endPage = realEnd;
+		    }
+
+		    this.prev = this.startPage > 1;
+
+		    this.next = this.endPage < realEnd;
 		  
 	  }
 }
--- miniproject/src/main/java/org/webservice/domain/boardsearch.java
+++ miniproject/src/main/java/org/webservice/domain/boardsearch.java
@@ -1,5 +1,14 @@
 package org.webservice.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
 public class boardsearch {
 	private int pageNum;
 	private int amount;
@@ -14,5 +23,21 @@ public class boardsearch {
 	public boardsearch(int pagenum,int amount) {
 		this.pageNum=pagenum;
 		this.amount=amount;
+	}
+	public String[] getTypeArr() {
+
+		return type == null ? new String[] {} : type.split("");
+	}
+	
+	public String getListLink() {
+
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("pageNum", this.pageNum)
+				.queryParam("amount", this.getAmount())
+				.queryParam("type", this.getType())
+				.queryParam("keyword", this.getKeyword());
+
+		return builder.toUriString();
+
 	}
 }
