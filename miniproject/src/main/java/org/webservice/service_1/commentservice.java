a4b070a27e3ade4958ffcaa50e8644a785b04ef8
somecreater <dbstjrgy123@gmail.com>
2023-12-22 16:58:37 +0900

    필요한 서비스 관련 인터페이스 등록

--- miniproject/src/main/java/org/webservice/service_1/commentservice.java
+++ miniproject/src/main/java/org/webservice/service_1/commentservice.java
@@ -1,5 +1,18 @@
 package org.webservice.service_1;
 
-public interface commentservice {
+import java.util.List;
 
+import org.webservice.domain.boardsearch;
+import org.webservice.domain.comment;
+import org.webservice.domain.commentpage;
+
+public interface commentservice {
+	
+	public int registercomment(comment cmt);
+	public comment getcomment(Long rno);
+	public int updatecomment(comment cmt);
+	public int deletecomment(Long rno);
+	
+	public List<comment> getcmtlist(boardsearch search, Long bno);
+	public commentpage getcmtlistpage(boardsearch search, Long bno);
 }
