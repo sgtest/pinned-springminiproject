1f002c6711f6e5827be1dd86f797a1a1ef9149ed
somecreater <dbstjrgy123@gmail.com>
2023-12-22 18:29:17 +0900

    게시판 서비스 메소드 등록

--- miniproject/src/main/java/org/webservice/mapper/boardmapper.java
+++ miniproject/src/main/java/org/webservice/mapper/boardmapper.java
@@ -1,5 +1,26 @@
 package org.webservice.mapper;
 
-public interface boardmapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.webservice.domain.attachfile;
+import org.webservice.domain.board;
+import org.webservice.domain.boardsearch;
+
+public interface boardmapper {
+	public int banuser(String userid);
+	
+	public int createboard(String boardname, String boardsubject);
+	
+	public board readboard(Long bno);
+	public int deleteboard(Long bno);
+	public int updateboard(board bd);
+	public void insertboard(board bd);
+	public List<board> getlistboard();
+	public List<board> getlistsearchboard(boardsearch search);
+	
+	
+	public void updatecmtcnt(@Param("bno") Long bno, @Param("amount") int amount);
+	public int gettotalcntboard(boardsearch search);
+	public List<attachfile> findfilebybno(Long bno);
 }
