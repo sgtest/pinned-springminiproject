7b223bfc8c8285c5010d3c50526de5568345ab6d
somecreater <dbstjrgy123@gmail.com>
2023-12-29 19:33:23 +0900

    사용될 모든 sql문 설계 완료

--- miniproject/src/main/java/org/webservice/mapper/commentmapper.java
+++ miniproject/src/main/java/org/webservice/mapper/commentmapper.java
@@ -9,7 +9,7 @@ import org.webservice.domain.comment;
 public interface commentmapper {
 	public comment readcomment(Long bno);
 	public int insertcomment(comment cmt);
-	public int deletecomment(Long bno);
+	public int deletecomment(Long rno);
 	public int updatecomment(comment cmt);
 	
 	public List<comment> getlistcomment(@Param("search")boardsearch search,@Param("bno") Long bno);
