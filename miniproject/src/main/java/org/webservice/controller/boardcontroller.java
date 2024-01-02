7994068527399923f6a2498c8b9caaf7d53fbd12
somecreater <dbstjrgy123@gmail.com>
2024-01-03 00:51:44 +0900

    컨트롤러 구현(좀더 관련부분 복습)

--- miniproject/src/main/java/org/webservice/controller/boardcontroller.java
+++ miniproject/src/main/java/org/webservice/controller/boardcontroller.java
@@ -2,6 +2,8 @@ package org.webservice.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,22 +17,37 @@ import lombok.extern.log4j.Log4j;
 public class boardcontroller {
 
 	
-	@RequestMapping(value="/readBoard/",method=RequestMethod.GET)
+	@GetMapping("/readBoard")
 	public void readboardLink() {
 		
 	}
 	
 	@PreAuthorize("authenticated()")
-	@RequestMapping(value="/createBoard/",method=RequestMethod.POST)
-	public String createboardLink() {
+	@PostMapping("/createBoardlist")
+	public String createboardlist() {
 		
 		return "redirect:/board/list";
 	}
 	
 	@PreAuthorize("authenticated()")
-	@RequestMapping(value="/updateBoard/",method=RequestMethod.POST)
+	@PostMapping("/createBoard")
+	public String createboardLink() {
+		
+		return "redirect:/board/list";
+	}
+	
+	@PreAuthorize("principal.username == #board.writer")
+	@PostMapping("/updateBoard")
 	public String updateboardLink(){
 		
 		return "redirect:/board/list";
 	}
+	
+	@PreAuthorize("principal.username == #board.writer")
+	@PostMapping("/removeBoard")
+	public String deleteboard() {
+		return "redirect:/board/list";
+	}
+	
+	
 }
