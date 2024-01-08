a90c913306da7db29f9b59a322483376c64775ba
somecreater <dbstjrgy123@gmail.com>
2024-01-09 02:04:12 +0900

    sql 맵퍼와 sql 문 자체의 문제인듯?

--- miniproject/src/test/java/org/webservice/service_1/boardservicetest.java
+++ miniproject/src/test/java/org/webservice/service_1/boardservicetest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.webservice.domain.board;
 import org.webservice.mapper.boardmapper;
+import org.webservice.mapper.membermapper;
 import org.webservice.persis.DataSourceTests;
 
 import lombok.Setter;
@@ -20,17 +21,23 @@ public class boardservicetest {
 //board 서비스와 file 서비스를 테스트 하는 코드입니다
 	
 	@Setter(onMethod_= {@Autowired})
-	public boardservice bservice;
+	public boardmapper bmapper;
+
+	//@Test
+	public void testmemberinfo() {
+		log.info(bmapper.select_boardaouth("main"));
+	}
 	
 	@Test
 	@DisplayName("게시판 생성 테스트")
 	public void testregisterboard() {
-		bservice.board_register("main","master","this is main board");
+		bmapper.createboard("main", "master", "this is main board");
 	}
 	
 	//@Test
 	public void testinsertboard() {
 		
 	}
+	
 	
 }
