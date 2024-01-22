dfe40ecf7e2d47d1be3ab7bea4eb07f27d877a05
somecreater <dbstjrgy123@gmail.com>
2024-01-22 16:33:55 +0900

    댓글목록 화면출력 성공

--- miniproject/src/main/java/org/webservice/controller/commentcontroller.java
+++ miniproject/src/main/java/org/webservice/controller/commentcontroller.java
@@ -3,6 +3,8 @@ package org.webservice.controller;
 import java.util.List;
 
 import org.apache.ibatis.javassist.expr.NewArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
