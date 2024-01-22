6519e29873312d80278682c7e70f75fd62825dc2
somecreater <dbstjrgy123@gmail.com>
2024-01-22 15:16:12 +0900

    댓글 기능 구현하는중 (자스부분에서 오류 발생)

--- miniproject/src/main/java/org/webservice/controller/commentcontroller.java
+++ miniproject/src/main/java/org/webservice/controller/commentcontroller.java
@@ -1,8 +1,21 @@
 package org.webservice.controller;
 
+import java.util.List;
+
+import org.apache.ibatis.javassist.expr.NewArray;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.webservice.domain.boardsearch;
+import org.webservice.domain.comment;
+import org.webservice.domain.commentpage;
 import org.webservice.service_1.boardservice;
+import org.webservice.service_1.commentservice;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -14,7 +27,42 @@ import lombok.extern.log4j.Log4j;
 public class commentcontroller {
 	
 	
-	public void insertcomment() {
+	boardservice bservice;
+	
+	commentservice cservice;
+	
+	//@PreAuthorize("authenticated()")
+	@PostMapping("/insertcomment")
+	public void insertcomment(comment cmt, RedirectAttributes rttr) {
+		cservice.registercomment(cmt);
+		rttr.addFlashAttribute("result", "success");
+	}
+	
+	//@PreAuthorize("principal.username == #board.writer")
+	@PostMapping("/deletecomment")
+	public void deletecomment(Long rno, RedirectAttributes rttr) {
+		if(cservice.deletecomment(rno)==1) {
+			rttr.addFlashAttribute("result", "success");
+		}
 		
 	}
+	
+	@PostMapping("/updatecomment")
+	public void updatecomment(comment cmt, RedirectAttributes rttr) {
+		if(cservice.updatecomment(cmt)==1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+	}
+	
+	@GetMapping("/readcommentlist")
+	@ResponseBody
+	public commentpage readcommentlist(@RequestParam int page,@RequestParam Long bno) {
+		boardsearch search=new boardsearch(page,10);
+		List<comment> cmtlist=cservice.getcmtlist(search, bno);
+		commentpage cmtpage=new commentpage(cmtlist.size(), cmtlist);
+		
+		return cmtpage;
+	}
+	
 }
