13047e4b1ec08c1c9a0d37c0345c87886aa060e2
somecreater <dbstjrgy123@gmail.com>
2024-02-04 02:45:59 +0900

    회원 기능을 추가로 구현 예정

--- miniproject/src/main/java/org/webservice/controller/securitycontroller.java
+++ miniproject/src/main/java/org/webservice/controller/securitycontroller.java
@@ -1,5 +1,35 @@
 package org.webservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class securitycontroller {
+
+@GetMapping("/loginboard")
+public void loginview() {
+	
+}
+@PostMapping("/loginaction")
+public String loginaction() {
+	return null;
+}
+@GetMapping("/loginerror")
+public void loginerror() {
+	
+}
+
+@PostMapping("/logoutaction")
+public String logoutaction() {
+	return null;
+}
+
+@GetMapping("/boardjoin")
+public void boardjoin() {
+	
+}
+@PostMapping("/boardjoinaction")
+public void boardjoinaction() {
+	
+}
 
 }
--- miniproject/src/main/java/org/webservice/mapper/membermapper.java
+++ miniproject/src/main/java/org/webservice/mapper/membermapper.java
@@ -1,7 +1,15 @@
 package org.webservice.mapper;
 
+import java.util.List;
+
+import org.webservice.domain.auth;
 import org.webservice.domain.member;
 
 public interface membermapper {
 	public member readmember(String userid);
+	public int insertmember(member member);
+	public int updatemember(member member);
+	public int deletemember(member member);
+	public List<auth> readauth(String userid);
+	public List<member> memberlist();
 }
